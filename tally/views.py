0755d6e3280b485c17646fc8dc28f97e3505f2ad
rguo123 <richardg7890@gmail.com>
2019-03-13 17:39:38 -0400

    splash page tempalte from colorlib, replace later

--- tally/views.py
+++ tally/views.py
@@ -1,6 +1,12 @@
 from tally import app, db
-
+from flask import render_template
 ### Test Server ###
 @app.route('/helloworld')
 def hello_world():
     return "Hello World!"
+
+
+### splash page
+@app.route('/')
+def index():
+    return render_template("index.html")
