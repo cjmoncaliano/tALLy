8755361be5cd6404df7f60115ae5256b06f191a8
rguo123 <richardg7890@gmail.com>
2019-04-17 15:35:16 -0400

    login, registration, and logout views

--- tally/forms.py
+++ tally/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, PasswordField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class ApplicantForm(FlaskForm):
@@ -9,3 +9,14 @@ class ApplicantForm(FlaskForm):
     major = StringField('Major(s)/Minor(s)', validators=[DataRequired()])
     phone = StringField('Phone Number', validators=[DataRequired()])
     submit = SubmitField('Done')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    role = SelectField('Role', choices =[('student', 'Student'), ('recruiter', 'Recruiter')], validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
--- tally/views.py
+++ tally/views.py
@@ -1,11 +1,15 @@
-from tally import app, db
-from tally.forms import ApplicantForm
-from flask import render_template, redirect
-### Test Server ###
-@app.route('/helloworld')
-def hello_world():
-    return "Hello World!"
-
+from tally import app, db, login_manager
+from tally.forms import ApplicantForm, RegistrationForm, LoginForm
+from flask import render_template, redirect, url_for, request
+from flask_login import login_user, logout_user, login_required, current_user
+import uuid
+### Login ###
+@login_manager.user_loader
+def load_user(userID):
+    user = db.users.find_one({"id": str(userID)})
+    if not user:
+        return None
+    return User(user["id"], user["role"])
 
 ### splash page
 @app.route('/')
@@ -16,9 +20,44 @@ def index():
 def login():
     return render_template("login.html")
 
+@app.route('/login_user')
+def login_user():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # TODO: change this to hash and salt
+        user = db.users.find_one({"username":form.username.data, \
+        "password": form.password.data})
+        if user is None:
+            return "Invalid Credentials"
+        else:
+            user_obj = User(user["id"], user["role"])
+            login_user(user_obj)
+            return redirect(url_for('/profile'))
+    return redirect(url_for('login_user'))
+
+
 @app.route('/create')
 def create():
     return render_template("create_account.html")
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = db.users.find_one({"username": form.username.data})
+        if user:
+            return "Error: username taken"
+        id = str(uuid.uuid4())
+        db.users.insert_one({
+            "username": form.username.data,
+            # TODO: change this to hash and salt
+            "password": form.password.data,
+            "id": id,
+        })
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('create'))
+
 
 @app.route('/input_resume', methods=['GET', 'POST'])
 def input_resume():
@@ -29,10 +68,20 @@ def input_resume():
         return redirect('/') #redirects to home screen
     return render_template("input_resume.html", form=form)
 
-@app.route('/register')
-def register():
-    return render_template("register.html")
 
 @app.route('/profile')
+@login_required
 def profile():
     return render_template("profile.html")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+### Test Server ###
+@app.route('/helloworld')
+def hello_world():
+    return "Hello World!"
