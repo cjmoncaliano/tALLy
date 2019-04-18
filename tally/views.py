b19e270f04b0c39e8b0a0ef11ba8e36615f2f3eb
rguo123 <richardg7890@gmail.com>
2019-04-18 12:10:25 -0400

    login and registration working

--- tally/views.py
+++ tally/views.py
@@ -3,6 +3,7 @@ from tally.forms import ApplicantForm, RegistrationForm, LoginForm
 from flask import render_template, redirect, url_for, request
 from flask_login import login_user, logout_user, login_required, current_user
 import uuid
+from tally.models import User
 ### Login ###
 @login_manager.user_loader
 def load_user(userID):
@@ -16,15 +17,10 @@ def load_user(userID):
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
-
-@app.route('/login_user')
-def login_user():
     form = LoginForm()
-    if form.validate_on_submit():
-        # TODO: change this to hash and salt
+    if request.method == 'POST' and form.validate_on_submit():
         user = db.users.find_one({"username":form.username.data, \
         "password": form.password.data})
         if user is None:
@@ -32,19 +28,36 @@ def login_user():
         else:
             user_obj = User(user["id"], user["role"])
             login_user(user_obj)
-            return redirect(url_for('/profile'))
+            return redirect(url_for('profile'))
     else:
+        print(form.errors)
+        return render_template("login.html", form=form)
+
+
+@app.route('/register', methods = ["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        user = db.users.find_one({"username": form.username.data})
+        if user:
+            return "Error: username taken"
+        id = str(uuid.uuid4())
+        db.users.insert_one({
+            "username": form.username.data,
+            # TODO: change this to hash and salt
+            "password": form.password.data,
+            "role": form.role.data,
+            "id": id,
+        })
         return redirect(url_for('login'))
-
-
-@app.route('/create')
-def create():
-    return render_template("create_account.html")
+    else:
+        print(form.errors)
+        return render_template('create_account.html', form = form)
 
 @app.route('/input_resume')
 def input_resume():
     return render_template('input_resume.html')
-
+"""
 @app.route('/register')
 def register():
     form = RegistrationForm()
@@ -61,11 +74,11 @@ def register():
         })
         return redirect(url_for('login'))
     else:
-        return redirect(url_for('create'))
+        return redirect(url_for('create')) """
 
 
-@app.route('/input_resume', methods=['GET', 'POST'])
-def input_resume():
+@app.route('/submit_resume', methods=['GET', 'POST'])
+def submit_resume():
     form = ApplicantForm()
     print(form.validate_on_submit()) #returns false
     if form.is_submitted(): #submitting without validating
