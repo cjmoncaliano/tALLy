5e4e5c4de2c257b685bd4499c282120b07e5eabc
Claudia J Moncaliano <claudiajmoncaliano@gmail.com>
2019-04-18 00:30:15 -0400

    one work exp, one activity, end to end

--- tally/forms.py
+++ tally/forms.py
@@ -2,10 +2,32 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
+class WorkExperience(FlaskForm):
+    company = StringField('Company Name')
+    
+    role = StringField('Role/Title')
+    
+    desc = StringField('Description')
+    
+class ExtraActivity(FlaskForm):
+    group = StringField('Company Name')
+    
+    role = StringField('Role/Title')
+    
+    desc = StringField('Description')
+
 class ApplicantForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
+    
     school = StringField('School', validators=[DataRequired()])
+    
     gpa = StringField('GPA', validators=[DataRequired()])
+    
     major = StringField('Major(s)/Minor(s)', validators=[DataRequired()])
+    
     phone = StringField('Phone Number', validators=[DataRequired()])
+    
     submit = SubmitField('Done')
+    
+    
--- tally/views.py
+++ tally/views.py
@@ -1,5 +1,5 @@
 from tally import app, db
-from tally.forms import ApplicantForm
+from tally.forms import ApplicantForm, WorkExperience, ExtraActivity
 from flask import render_template, redirect
 ### Test Server ###
 @app.route('/helloworld')
@@ -23,11 +23,13 @@ def create():
 @app.route('/input_resume', methods=['GET', 'POST'])
 def input_resume():
     form = ApplicantForm()
+    work = WorkExperience()
+    activity = ExtraActivity()
     print(form.validate_on_submit()) #returns false
     if form.is_submitted(): #submitting without validating
-        print(form.school.data, form.gpa.data, form.major.data, form.email.data, form.phone.data)
+        print(form.school.data, form.gpa.data, form.major.data, form.email.data, form.phone.data, work.company.data, activity.role.data)
         return redirect('/') #redirects to home screen
-    return render_template("input_resume.html", form=form)
+    return render_template("input_resume.html", form=form, work=work, activity=activity)
 
 @app.route('/register')
 def register():
