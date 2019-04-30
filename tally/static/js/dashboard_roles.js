d261fea0a226086e0fc1220f5490cf22f0883218
Claudia J Moncaliano <claudiajmoncaliano@gmail.com>
2019-04-30 18:46:44 -0400

    intermediary dynamic table

---
+++ tally/static/js/dashboard_roles.js
@@ -0,0 +1,9 @@
+function add_role() {
+  var table = document.getElementById("dashboard_table");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+}
+add_role()
