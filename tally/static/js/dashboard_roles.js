7e66fd2dee9a5f156b670cec245368a8c88e0c6b
Claudia J Moncaliano <claudiajmoncaliano@gmail.com>
2019-05-01 16:58:14 -0400

    routes imporved, js dynamic role pop

--- tally/static/js/dashboard_roles.js
+++ tally/static/js/dashboard_roles.js
@@ -1,9 +1,14 @@
 function add_role() {
-  var table = document.getElementById("dashboard_table");
-  var row = table.insertRow(0);
+  var table = document.getElementById("dashboard_table").getElementsByTagName('tbody')[0];
+  var row = table.insertRow(table.rows.length);
+    
   var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
+  var newText = document.createTextNode(user_info.open_roles[1].role);
+  cell1.appendChild(newText);
+    
+  //var cell2 = row.insertCell(1);
+  //cell1.innerHTML = user_info.open_roles[1].role};
+  //cell2.innerHTML = "NEW CELL2";
 }
+
 add_role()
